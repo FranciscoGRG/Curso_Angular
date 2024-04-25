2917ddd8e5aa442fc7c1af143815c1d64e21e250
FranciscoGRG <fgrcalifa@gmail.com>
2024-04-25 12:22:08 +0200

    completado hasta el video 9

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { routes } from '../../app.routes';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './sidemenu.component.html',
+  styleUrl: './sidemenu.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SidemenuComponent { 
+
+  public menuItems = routes.map(
+    route => route.children ?? []
+  ).flat()
+  .filter(route => route && route.path)
+  .filter(route => !route.path?.includes(':'));
+
+  constructor() { 
+
+  }
+}
