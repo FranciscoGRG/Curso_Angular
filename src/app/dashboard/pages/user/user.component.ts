f0e15fbcca3c2a52f77341d240a960c6a879eb51
FranciscoGRG <fgrcalifa@gmail.com>
2024-05-08 13:08:21 +0200

    Curso Angular Terminado

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,14 +1,47 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import TitleComponent from '../../../shared/title/title.component';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../../../interfaces/req-response';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
+import { UsersService } from '../../../services/users.service';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [
-    CommonModule,
+    CommonModule, TitleComponent,
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class UserComponent { }
+export default class UserComponent {
+
+  private route = inject(ActivatedRoute);
+  private usersService = inject(UsersService);
+
+  public titleLabel = computed(() => {
+    if (this.user()) {
+      return `Informacion del usuario: ${this.user()?.first_name} ${this.user()?.last_name}`;
+    }
+
+    return 'Informacion del usuario';
+  })
+
+
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap(({ id }) => this.usersService.getUserById(id))
+    )
+  );
+
+
+  // constructor() {
+  //   this.route.params.subscribe(params => {
+  //     console.log({ params });
+  //   })
+  // }
+
+}
--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,14 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { UsersService } from '../../../services/users.service';
+import TitleComponent from '../../../shared/title/title.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   standalone: true,
   imports: [
-    CommonModule,
+    CommonModule, TitleComponent, RouterModule,
   ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class UsersComponent { }
+export default class UsersComponent { 
+
+  public usersService = inject(UsersService)
+
+
+
+
+}
+
